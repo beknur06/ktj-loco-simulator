290b7220e87277c8fdfee3e99588d82a424f0479
Beknur <beknur.les06@gmail.com>
2026-04-04 13:05:20 +0500

    Bismillah

---
+++ src/main/java/kz/mz/exd/simulatorservice/SimulatorServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.mz.exd.simulatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/mz/exd/simulatorservice/SimulatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.mz.exd.simulatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
