fe31f8429fc05bd1cb31b8c4604efc580f990601
Beknur <beknur.les06@gmail.com>
2026-04-04 13:05:54 +0500

    Bismillah

--- src/main/java/kz/mz/exd/simulatorservice/SimulatorServiceApplication.java
+++ src/main/java/kz/mz/exd/simulatorservice/SimulatorServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SimulatorServiceApplication {
 
 	public static void main(String[] args) {
+        System.out.println("Starting Simulator Service Application...");
 		SpringApplication.run(SimulatorServiceApplication.class, args);
 	}
 
